0df56437ca71f46bf38132dcb2777f835a76734c
AnastasiaCoder <mstitelnica1999@gmail.com>
2022-02-26 14:16:54 +0700

    added tests

---
+++ src/test/java/gmail/anastasiacoder/tests/TestBase.java
@@ -0,0 +1,45 @@
+package gmail.anastasiacoder.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import gmail.anastasiacoder.config.RunConfig;
+import gmail.anastasiacoder.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static gmail.anastasiacoder.helpers.RunHelper.runHelper;
+
+public class TestBase {
+    private final RunConfig config = ConfigFactory.create(RunConfig.class, System.getProperties());
+
+    @BeforeAll
+    public static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = runHelper().getDriverClass().getName();
+        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+        Configuration.timeout = 10000;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+
+        if (config.deviceHost().equals("selenoid")) {
+            Attach.addVideo();
+        }
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/gmail/anastasiacoder/tests/WikipediaTests.java
@@ -0,0 +1,89 @@
+package gmail.anastasiacoder.tests;
+
+
+import gmail.anastasiacoder.annotations.JiraIssue;
+import gmail.anastasiacoder.annotations.JiraIssues;
+import gmail.anastasiacoder.annotations.Layer;
+import gmail.anastasiacoder.annotations.Microservice;
+import io.appium.java_client.MobileBy;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+@Layer("UI")
+@Story("Wikipedia")
+@Feature("Browserstack")
+@JiraIssues({@JiraIssue("HOMEWORK-336")})
+@Tags({@Tag("Browserstack"),@Tag("UI")})
+public class WikipediaTests extends TestBase{
+
+    @Test
+    @DisplayName("Проверка результата поиска по значению 'BrowserStack'")
+    @Microservice("Search")
+    @Feature("Поиск")
+    @Owner("AnotherQA")
+    @Severity(SeverityLevel.CRITICAL)
+    void searchBrowserStackInWikipedia() {
+        back();
+        step("Установка курсора в поле поиска", () ->
+                $(MobileBy.AccessibilityId("Search Wikipedia")).click());
+        step("Ввод значения 'BrowserStack' и инициация поиска", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack"));
+        step("Проверка наличия результатов", () -> {
+            $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка открытия настроек")
+    @Microservice("Settings")
+    @Feature("Настройки")
+    @Owner("Ambidre")
+    @Severity(SeverityLevel.BLOCKER)
+    void openSettingsTest() {
+        step("Открыть приложение", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/menu_overflow_button")).click()
+        );
+
+        step("Открыть меню Settings", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/explore_overflow_settings")).click()
+        );
+
+        step("Проверка: меню Settings открыто", () -> {
+            $(MobileBy.xpath(("//*[@text='General']"))).shouldBe(visible);
+            $(MobileBy.xpath(("//*[@text='Privacy']"))).shouldBe(visible);
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка страниц getting started")
+    @Microservice("Getting started")
+    @Feature("Начало работы")
+    @Owner("Ambidre")
+    @Severity(SeverityLevel.CRITICAL)
+    void gettingStartedWikipediaTest() {
+        back();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("New ways to explore"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("Reading lists with sync"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                .shouldHave(text("Send anonymous data"));
+        $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_done_button")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/search_container"))
+                .shouldBe(value(String.valueOf(visible)));
+    }
+}
